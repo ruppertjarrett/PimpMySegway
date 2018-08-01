6fb60d2d47ebf53cced304878e6b14b76f443e85
Jesus Ledezma <jledezma@student.neumont.edu>
2018-07-31 23:23:54 -0600

    Final commit

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ add_nav();
 add_containers();
 add_footer();
 
+var image = document.getElementById('image');
 var totalCost = 0;
 
 function load(evt) {
     totalCost = 600;
-    var image = document.getElementById('image');
     image.style.background = "url('images/Empty.png'), url('images/Tires/Basic.png'), url('images/Body/Whi.png')";
+    document.getElementById("price").textContent = "Total: $" + totalCost;
     image.style.backgroundRepeat = "no-repeat";
     image.style.backgroundSize = "cover";
-    document.getElementById("price").textContent = "Total: $" + totalCost;
 }
 
 function add_nav() {
@@ -41,7 +41,7 @@ function add_nav() {
     var preset_1 = document.createElement("div");
     preset_1.setAttribute("id", "preset_1");
     li_1.appendChild(preset_1);
-    var preset_1_placeholder = document.createTextNode("Roadster");
+    var preset_1_placeholder = document.createTextNode("The Roadster");
     var preset_1_div = document.getElementById("preset_1");
     preset_1.appendChild(preset_1_placeholder);
 
@@ -52,7 +52,7 @@ function add_nav() {
     var preset_2 = document.createElement("div");
     preset_2.setAttribute("id", "preset_2");
     li_2.appendChild(preset_2);
-    var preset_2_placeholder = document.createTextNode("Huntsman");
+    var preset_2_placeholder = document.createTextNode("The Huntsman");
     var preset_2_div = document.getElementById("preset_2");
     preset_2.appendChild(preset_2_placeholder);
 
@@ -74,7 +74,7 @@ function add_nav() {
     var preset_4 = document.createElement("div");
     preset_4.setAttribute("id", "preset_4");
     li_4.appendChild(preset_4);
-    var preset_4_placeholder = document.createTextNode("Hipster");
+    var preset_4_placeholder = document.createTextNode("The Hipster");
     var preset_4_div = document.getElementById("preset_4");
     preset_4.appendChild(preset_4_placeholder);
 
@@ -85,7 +85,7 @@ function add_nav() {
     var preset_5 = document.createElement("div");
     preset_5.setAttribute("id", "preset_5");
     li_5.appendChild(preset_5);
-    var preset_5_placeholder = document.createTextNode("Total Package");
+    var preset_5_placeholder = document.createTextNode("TOTAL PACKAGE");
     var preset_5_div = document.getElementById("preset_5");
     preset_5.appendChild(preset_5_placeholder);
 }
@@ -138,7 +138,7 @@ function add_footer() {
     var item_list = document.createElement("div");
     item_list.setAttribute("id", "item_list");
     leftFooter.appendChild(item_list);
-    var item_list_placeholder = document.createTextNode("item list goes here");
+    var item_list_placeholder = document.createTextNode("Item List: \n");
     var item_list_div = document.getElementById("item_list");
     item_list_div.appendChild(item_list_placeholder);
 
@@ -159,11 +159,13 @@ function add_footer() {
 
     var checkout = document.createElement("div");
     checkout.setAttribute("id", "checkout");
+    checkout.setAttribute('class', 'btn');
     rightFooter.appendChild(checkout);
-    var checkout_placeholder = document.createTextNode("checkout goes here");
+    var checkout_placeholder = document.createTextNode("Checkout");
     var checkout_div = document.getElementById("checkout");
     checkout_div.appendChild(checkout_placeholder);
 }
+document.getElementById("checkout").addEventListener("click", checkout);
 
 var Options = document.getElementById('options');
 var BtnId = ['Red', 'White', 'Blue', 'Silver', 'SafetyOrange', 'OffRoad', 'Basic', 'Racing', 'V2', 'V4', 'V8', 'Tassels', 'Mirrors', 'LEDStrip', 'Speaker', 'Cupholder', 'GPS', 'Bell', 'Headlights'];
@@ -203,7 +205,7 @@ function buildButton(item, index, arr) {
         Options.appendChild(container);
     } else if (overallCount == 0 && extrasCount == 0) {
         container = document.createElement('div');
-        container.setAttribute('id', 'Extras');
+        container.setAttribute('id', 'extras');
         namePlate = document.createElement('p');
         namePlate.innerHTML = `Extras :`;
         container.appendChild(namePlate);
@@ -224,7 +226,8 @@ function buildButton(item, index, arr) {
         Btns[index].setAttribute('class', 'btn extra');
         extrasCount++;
     }
-    Options.appendChild(Btns[index]);
+    Btns[index].innerHTML = BtnId[index];
+    container.appendChild(Btns[index]);
     overallCount++;
     if (colorCount == 5) {
         Options.innerHTML += '<br />';
@@ -254,35 +257,79 @@ var currentBtn;
 var currentParent;
 var option;
 var area;
+var cost;
 
 function optionClick(evt) {
     currentBtn = evt.target.id;
     option = currentBtn;
-    imgURL;
+    var imgURL;
     if (option == 'Red' || option == 'White' || option == 'Silver' || option == 'Blue' || option == 'SafetyOrange') {
-        color = image.style.background.substring(image.style.background.length - 28, image.style.background.length);
+        var color = image.style.background.substring(image.style.background.length - 28, image.style.background.length);
+        console.log(color);
+        console.log(option);
         if (option == 'Red') {
+            cost = 100;
             imgURL = 'Red';
         } else if (option == 'White') {
+            cost = 100;
             imgURL = 'Whi';
         } else if (option == 'Blue') {
+            cost = 100;
             imgURL = 'Blu';
         } else if (option == 'Silver') {
+            cost = 100;
             imgURL = 'Sil';
         } else if (option == 'SafetyOrange') {
+            cost = 100;
             imgURL = 'Ora';
         }
-        image.style.background.replace(color, `, url('images/Body/${imgURL}.png)`);
+        console.log(imgURL);
+        if (document.getElementById('item_list').innerHTML.includes('Red')) {
+            document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace('Red $100', '');
+            totalCost += -100;
+        } else if (document.getElementById('item_list').innerHTML.includes('White')) {
+            document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace('White $100', '');
+            totalCost += -100;
+        } else if (document.getElementById('item_list').innerHTML.includes('Blue')) {
+            document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace('Blue $100', '');
+            totalCost += -100;
+        } else if (document.getElementById('item_list').innerHTML.includes('Silver')) {
+            document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace('Silver $100', '');
+            totalCost += -100;
+        } else if (document.getElementById('item_list').innerHTML.includes('SafetyOrange')) {
+            document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace('SafetyOrange $100', '');
+            totalCost += -100;
+        }
+        image.style.background = image.style.background.replace(color, `, url('images/Body/${imgURL}.png')`)
+        document.getElementById("item_list").innerHTML += `${option} $${cost} \<br /\>`;
+        totalCost += cost;
+        document.getElementById("price").innerHTML = 'Total Cost: ' + totalCost;
     } else if (option == 'OffRoad' || option == 'Basic' || option == 'Racing') {
-        tire = image.style.background.substring(image.style.background.length - 59, image.style.background.length - 28);
+        var tire = image.style.background.substring(image.style.background.length - 59, image.style.background.length - 28);
         if (option == 'Basic') {
+            cost = 100
             imgURL = 'Basic';
         } else if (option == 'OffRoad') {
+            cost = 300
             imgURL = 'OffRd';
         } else if (option == 'Racing') {
+            cost = 300
             imgURL = 'Racin';
         }
-        image.style.background.replace(tire, `, url('images/Tires/${imgURL}.png)`);
+        if (document.getElementById('item_list').innerHTML.includes('Basic')) {
+            document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace('Basic $100', '');
+            totalCost += -100;
+        } else if (document.getElementById('item_list').innerHTML.includes('OffRoad')) {
+            document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace('OffRoad $300', '');
+            totalCost += -300;
+        } else if (document.getElementById('item_list').innerHTML.includes('Racing')) {
+            document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace('Racing $300', '');
+            totalCost += -300;
+        }
+        image.style.background = image.style.background.replace(tire, `, url('images/Tires/${imgURL}.png')`);
+        document.getElementById("item_list").innerHTML += `${option} $${cost} \<br /\>`;
+        totalCost += cost;
+        document.getElementById("price").innerHTML = 'Total Cost: ' + totalCost;
     } else if (option == 'V2' || option == 'V4' || option == 'V8') {
         if (option == 'V2') {
             cost = 300
@@ -291,22 +338,74 @@ function optionClick(evt) {
         } else if (option == 'V8') {
             cost = 1200
         }
+        if (document.getElementById('item_list').innerHTML.includes('V2')) {
+            document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace('V2 $300', '');
+            totalCost += -300;
+        } else if (document.getElementById('item_list').innerHTML.includes('V4')) {
+            document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace('V4 $600', '');
+            totalCost += -600;
+        } else if (document.getElementById('item_list').innerHTML.includes('V8')) {
+            document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace('V8 $1200', '');
+            totalCost += -1200;
+        }
+        document.getElementById("item_list").innerHTML += `${option} $${cost} \<br /\>`;
+        totalCost += cost;
+        document.getElementById("price").innerHTML = 'Total Cost: ' + totalCost;
     } else {
-        imgURL = option;
-        image.style.background += `, url('images/Extra/${imgURL}.png)`;
+        if (document.getElementById('item_list').innerHTML.includes(option)) {
+            if (option == 'Bell' || option == 'Tassels') {
+                totalCost += -20;
+                document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace(`${option} $20`, '');
+            } else if (option == 'Cupholder' || option == 'Mirrors') {
+                totalCost += -40;
+                document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace(`${option} $40`, '');
+            } else if (option == 'LEDStrip' || option == 'Headlights') {
+                totalCost += -80;
+                document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace(`${option} $80`, '');
+            } else if (option == 'Speaker' || option == 'GPS') {
+                totalCost += -120;
+                document.getElementById('item_list').innerHTML = document.getElementById("item_list").innerHTML.replace(`${option} $120`, '');
+            }
+            image.style.background = image.style.background.replace(`, url("images/Extra/${option}.png")`, '');
+            document.getElementById("price").innerHTML = 'Total Cost: ' + totalCost;
+        } else {
+            if (option == 'Bell' || option == 'Tassels') {
+                cost = 20;
+            } else if (option == 'Cupholder' || option == 'Mirrors') {
+                cost = 40;
+            } else if (option == 'LEDStrip' || option == 'Headlights') {
+                cost = 80;
+            } else if (option == 'Speaker' || option == 'GPS') {
+                cost = 120;
+            }
+            imgURL = option;
+            basics = image.style.background.substring(image.style.background.length - 59, image.style.background.length);
+            image.style.background = image.style.background.substring(0, image.style.background.length - 59);
+            image.style.background += `, url('images/Extra/${imgURL}.png')`;
+            image.style.background += basics;
+            document.getElementById("item_list").innerHTML += `${option} $${cost} \<br /\>`;
+            totalCost += cost;
+            document.getElementById("price").innerHTML = 'Total Cost: ' + totalCost;
+        }
     }
-    document.getElementById("item_list").innerHTML += `${option} $${cost} \<br /\>`;
-    totalCost += cost;
-    document.getElementById("price").textContent = "Total: $" + totalCost;
 }
 
+function checkout(evt) {
+    alert("Thank you for your order!!!");
+    image.style.background = "url('images/Empty.png'), url('images/Tires/Basic.png'), url('images/Body/Whi.png')";
+    image.style.backgroundRepeat = "no-repeat";
+    image.style.backgroundSize = "cover";
+    document.getElementById('item_list').innerHTML = "Item List: ";
+    totalCost = 600
+    document.getElementById('price').innerHTML = "Total: $" + totalCost;
+}
 document.getElementById("preset_1").addEventListener('click', roadster);
 document.getElementById("preset_2").addEventListener('click', huntsman);
 document.getElementById("preset_3").addEventListener('click', plainJane);
 document.getElementById("preset_4").addEventListener('click', hipster);
-// document.getElementById("preset_5").addEventListener('click', totalPackage);
+document.getElementById("preset_5").addEventListener('click', totalPackage);
 
-function roadster(){
+function roadster() {
     var image = document.getElementById('image');
     image.style.background = "url('images/Empty.png'), url('images/Tires/Basic.png'), url('images/Body/Whi.png')";
     image.style.backgroundRepeat = "no-repeat";
@@ -319,7 +418,7 @@ function roadster(){
     image.style.backgroundSize = "cover";
 }
 
-function huntsman(){
+function huntsman() {
     var image = document.getElementById('image');
     image.style.background = "url('images/Empty.png'), url('images/Tires/Basic.png'), url('images/Body/Whi.png')";
     image.style.backgroundRepeat = "no-repeat";
@@ -332,7 +431,7 @@ function huntsman(){
     image.style.backgroundSize = "cover";
 }
 
-function plainJane(){
+function plainJane() {
     var image = document.getElementById('image');
     image.style.background = "url('images/Empty.png'), url('images/Tires/Basic.png'), url('images/Body/Whi.png')";
     image.style.backgroundRepeat = "no-repeat";
@@ -340,33 +439,30 @@ function plainJane(){
     totalCost = 500;
     document.getElementById("price").textContent = "Total: $" + totalCost;
     document.getElementById("item_list").innerHTML = "White $100 \<br/\> Basic $100 \<br/\> V2 $300 ";
-    image.style.background = "url('images/Empty.png'), url('images/Tires/Basic.png'), url('images/Body/Whi.png')";
-    image.style.backgroundRepeat = "no-repeat";
-    image.style.backgroundSize = "cover";
 }
 
-function hipster(){
+function hipster() {
     var image = document.getElementById('image');
     image.style.background = "url('images/Empty.png'), url('images/Tires/Basic.png'), url('images/Body/Whi.png')";
     image.style.backgroundRepeat = "no-repeat";
     image.style.backgroundSize = "cover";
     totalCost = 1060;
     document.getElementById("price").textContent = "Total: $" + totalCost;
-    document.getElementById("item_list").innerHTML = "Blue $100 \<br/\> Racing $300 \<br/\> V4 $600 \<br/\> Speaker $120 \<br/\> Cupholder $40 ";
+    document.getElementById("item_list").innerHTML = "Blue $100 <br/> Racing $300 <br/> V4 $600 <br/> Speaker $120 <br/> Cupholder $40 ";
     image.style.background = "url('images/Empty.png'), url('images/Extra/Speaker.png'), url('images/Extra/Cupholder.png'), url('images/Tires/Racin.png'), url('images/Body/Blu.png')";
     image.style.backgroundRepeat = "no-repeat";
     image.style.backgroundSize = "cover";
 }
 
-function totalPackage(){
+function totalPackage() {
     var image = document.getElementById('image');
     image.style.background = "url('images/Empty.png'), url('images/Tires/Basic.png'), url('images/Body/Whi.png')";
     image.style.backgroundRepeat = "no-repeat";
     image.style.backgroundSize = "cover";
     totalCost = 2720;
     document.getElementById("price").textContent = "Total: $" + totalCost;
-    document.getElementById("item_list").innerHTML = "Silver $100 \<br/\> Racing $300 \<br/\> V8 $1200 \<br/\> Speaker $120 \<br/\> GPS $120 \<br/\> LEDStrip $80\<br/\> Headlights $80\<br/\> Cupholder $40\<br/\> Mirros $40\<br/\> Tassels $20\<br/\> Bell $20";
-    image.style.background = "url('images/Empty.png'), url('images/Extra/Speaker.png'), url('images/Extra/Cupholder.png'), url('images/Tires/Racin.png'), url('images/Body/Blu.png')";
+    document.getElementById("item_list").innerHTML = "Silver $100 <br/> Racing $300 <br/> V8 $1200 <br/> Speaker $120 <br/> GPS $120 <br/> LEDStrip $80<br/> Headlights $80<br/> Cupholder $40<br/> Mirros $40<br/> Tassels $20<br/> Bell $20";
+    image.style.background = "url('images/Empty.png'), url('images/Extra/Headlights.png'), url('images/Extra/Tassels.png'), url('images/Extra/Bell.png'), url('images/Extra/Mirrors.png'), url('images/Extra/LEDStrip.png'), url('images/Extra/GPS.png'), url('images/Extra/Speaker.png'), url('images/Extra/Cupholder.png'), url('images/Tires/Racin.png'), url('images/Body/Sil.png')";
     image.style.backgroundRepeat = "no-repeat";
     image.style.backgroundSize = "cover";
 }
